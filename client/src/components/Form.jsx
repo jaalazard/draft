ad4ebdaff50430301e7f0dc28330f8afbd88f7b6
jaalazard <ja.alazard@proton.me>
2023-12-14 11:53:49 +0100

    first try with react hook form

---
+++ client/src/components/Form.jsx
@@ -0,0 +1,55 @@
+import react from "react";
+import { useForm } from "react-hook-form";
+
+const wait = function (duration = 1000) {
+  return new Promise((resolve) => {
+    window.setTimeout(resolve, duration);
+  });
+};
+
+const Form = () => {
+  const { register, handleSubmit, formState, formState: { errors } } = useForm({
+    mode: "onTouched",
+  });
+  const { isSubmitting, isSubmitted, isSubmitSuccessful } = formState;
+
+
+  const onSubmit = async (data) => {
+    await wait(2000);
+  };
+
+  console.log(errors);
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h1> Mon formulaire hook form</h1>
+{isSubmitSuccessful && <div>Merci pour tout!</div>}
+        <label htmlFor="firstname">Firstname</label>
+        <input
+          className="border form-control"
+          type="text"
+          id="firstname"
+          name="firstname"
+          {...register("firstname", { required: 'need your firstname', minLength: {value: 3, message: '+ de carac!'} })}
+        />
+        {errors.firstname && ( <span>{errors.firstname.message}</span>)}
+
+        <label htmlFor="lastname">Lastname</label>
+        <input
+          className="border form-control"
+          type="text"
+          id="lastname"
+          name="lastname"
+          {...register("lastname", { required: 'need your lastname', minLength: { value: 3, message: "need more cara!"} })}
+        />
+                {errors.lastname && ( <span>{errors.lastname.message}</span>)}
+
+
+        <button disabled={isSubmitting} type="submit">Submit</button>
+      </form>
+    </>
+  );
+};
+
+export default Form;
